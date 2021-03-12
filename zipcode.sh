f5add196ecd827310e95c85455f68d6534e08c1c
Zixuan Zhang <zhangzix@usc.edu>
2021-03-12 14:28:38 -0500

    update lambda policy + add new lambda layer

--- zipcode.sh
+++ zipcode.sh
@@ -1,6 +1,18 @@
 for lambda in src/*; do
   root=$(pwd)
   cd "$lambda"
-  zip -r code.zip *.py
+
+  if [ $lambda = "src/constants" ]
+  then
+    echo "compressing constant layer"
+    zip -r constant-layer.zip *
+  elif [ $lambda = "src/layers" ]
+  then
+    echo "skip src/layer folder"
+  else
+    echo "compressing lambda code"
+    zip -r code.zip *.py
+  fi
   cd "$root"
+
 done

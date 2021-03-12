27bdf1813c44803de1a6a018dcc7d64d906cb723
Linxiao Wu <linxiaow@umich.edu>
2021-03-12 10:53:34 -0500

    add persist to dynamoDB

---
+++ src/batchWriter/constants.py
@@ -0,0 +1,3 @@
+DYNAMODB_TABLE = "fraud-detection-sys-transaction-table"
+PARTITION_KEY = "user_id"
+SORT_KEY = "transaction_id"
--- src/batchWriter/handler.py
+++ src/batchWriter/handler.py
@@ -1,9 +1,25 @@
 import json
 import base64
 import datetime
+from constants import DYNAMODB_TABLE, PARTITION_KEY, SORT_KEY
 import boto3
 def handler(event, context):
     print(json.dumps(event))
+    msgs = event['records'].values()
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DYNAMODB_TABLE)
+    for lst in msgs:
+        for record in lst:
+            # print(record['value'], base64.b64decode(record['value']))
+            information = json.loads(base64.standard_b64decode(record['value']).decode('utf-8'))
+            print(information)
+            item = {}
+            # use SSN as user's SSN for now
+            item[PARTITION_KEY] = information["ssn"]
+            item[SORT_KEY] = information["trans_num"]
+            item["detail"] = json.dumps(information)
+            table.put_item(Item=item)
+            print("put record to dynamoDB")
     return {
         'statusCode': 200,
         'body': json.dumps("Hello World from batch writer !!!")
